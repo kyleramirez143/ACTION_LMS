a22347d76d6c288422b8f63758144ec26ceea77b
lloydadrl-aws <lloyd.lindo@awsys-i.com>
2025-11-24 15:56:15 +0800

    Added logout function in Navbar.jsx that actually clears the token.

--- frontend/lms-proj/src/App.jsx
+++ frontend/lms-proj/src/App.jsx
@@ -1,28 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Assessment from "./components/Assessment";
-import Course from "./components/Course";
-import Navbar from "./components/Navbar";
-import LoginPage from "./components/LoginPage";
-import QuizResult from "./components/QuizResult";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { AuthProvider } from './context/AuthContext.jsx'
-import ReviewPublish from "./components/ReviewPublish";
-
 import Navbar from "./all/Navbar";
 import LoginPage from "./all/LoginPage";
 import Assessment from "./trainer/Assessment";
-import AssessmentConfirmation from "./trainer/AssessmentConfirmation";
 import ReviewPublish from "./trainer/ReviewPublish";
 import Course from "./trainee/Course";
 import ModuleScreen from "./trainee/ModuleScreen"; 
 import AdminDashboard from './admin/AdminDashboard';
 //import StudentDashboard from './pages/TraineeDashboard';
-
-
 
 function AppContent() {
   const location = useLocation();
@@ -38,12 +28,9 @@ function AppContent() {
           <Route path="/modules" element={<ModuleScreen />} />
           <Route path="/course" element={<Course />} />
           <Route path="/assessment" element={<Assessment />} />
-          <Route path="/assessmentconfirmation" element={<AssessmentConfirmation />} />
           <Route path="/reviewpublish" element={<ReviewPublish />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           {/* <Route path="/student/dashboard" element={<StudentDashboard />} /> */}
-          <Route path="/quizresult" element={<QuizResult/>} />
-          <Route path="/reviewpublish" element={<ReviewPublish/>} />
         </Routes>
       </div>
     </>
