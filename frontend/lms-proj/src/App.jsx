dfd20d3bfe1a849c89a5b3a5b0f83be6c3d02616
lloydadrl-aws <lloyd.lindo@awsys-i.com>
2025-11-24 13:08:26 +0800

    New RBAC Schema.

--- frontend/lms-proj/src/App.jsx
+++ frontend/lms-proj/src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Assessment from "./components/Assessment";
-import Course from "./Components/Course";
-import Navbar from "./components/Navbar";
+import Course from "./components/Course";
 import LoginPage from "./components/LoginPage";
 import AdminDashboard from './pages/AdminDashboard';
 // import StudentDashboard from './pages/TraineeDashboard';
-import Navbar from "./Components/Navbar";
-import LoginPage from "./Components/LoginPage";
+import Navbar from "./components/Navbar";
 import AssessmentConfirmation from "./components/AssessmentConfirmation";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -25,7 +23,7 @@ function AppContent() {
 
       <div className="full-screen">
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          {/* <Route path="/" element={<LoginPage />} /> */}
           <Route path="/course" element={<Course />} />
           <Route path="/assessment" element={<Assessment />} />
           <Route path="/assessmentconfirmation" element={<AssessmentConfirmation />} />
@@ -37,16 +35,19 @@ function AppContent() {
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        {/* The target redirect route */}
-        <Route path="/admin/dashboard" element={<AdminDashboard />} /> 
-        {/* Other dashboard routes */}
-        {/* <Route path="/student/dashboard" element={<StudentDashboard />} /> */}
-        {/* ... */}
-      </Routes>
-    </Router>
+    <div className="full-screen">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          {/* The target redirect route */}
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          {/* Other dashboard routes */}
+          {/* <Route path="/student/dashboard" element={<StudentDashboard />} /> */}
+          {/* ... */}
+        </Routes>
+      </Router>
+    </div>
+
   );
 }
 
