8eaf31fd28e5ba25c4a63b3e4127a21a4183c162
ladymendoza18 <lady.mendoza@awsys-i.com>
2025-12-09 14:13:23 +0800

    upd

--- frontend/lms-proj/src/App.jsx
+++ frontend/lms-proj/src/App.jsx
@@ -10,30 +10,6 @@ import ProtectedRoute from "./components/ProtectedRoutes";
 // Navbar & Shared
 import Navbar from "./all/Navbar";
 import LoginPage from "./all/LoginPage";
-<<<<<<< HEAD
-import Assessment from "./trainer/Assessment";
-import AssessmentConfirmation from "./trainer/AssessmentConfirmation";
-import ReviewPublish from "./trainer/ReviewPublish";
-import Course from "./trainee/Course";
-import ModuleScreen from "./trainee/ModuleScreen";
-//import AdminDashboard from './pages/AdminDashboard';
-import TraineeDashboard from './trainee/TraineeDashboard';
-import AddPowerpoint from "./trainer/AddPowerpoint";
-import AddVideo from "./trainer/AddVideo";
-import AddActivity from "./trainer/AddActivity";
-import ProfileManagement from "./trainer/ProfileManagement";
-import AddCourse from "./admin/AddCourse";
-import AddModule from "./admin/AddModule";
-import AddUsers from "./admin/AddUsers";
-import UserRoleTable from "./admin/UserRoleTable";
-import CourseManagement from "./admin/CourseManagement";
-import ModuleManagement from "./admin/ModuleManagement";
-import Dashboard from "./trainer/Dashboard";
-import QuizResult from "./trainee/QuizResult";
-import CreateQuiz from "./trainer/CreateQuiz";
-import AdminDashboard from "./admin/AdminDashboard";
-=======
->>>>>>> e04b104a9210cc186fc966415cd794a316074cea
 
 // Admin Imports
 import AdminDashboard from './admin/AdminDashboard';
@@ -58,33 +34,6 @@ function AppContent() {
     <>
       {!hideNavbar && <Navbar />}
 
-<<<<<<< HEAD
-      <div className="container-fluid w-100 h-100">
-        <div className="row h-100">
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/modules" element={<ModuleScreen />} />
-            <Route path="/course" element={<Course />} />
-            <Route path="/assessment" element={<Assessment />} />
-            <Route path="/assessmentconfirmation" element={<AssessmentConfirmation />} />
-            <Route path="/reviewpublish" element={<ReviewPublish />} />
-            <Route path="/trainer/addpowerpoint" element={<AddPowerpoint />} />
-            <Route path="/trainer/addvideo" element={<AddVideo />} />
-            <Route path="/trainer/addactivity" element={<AddActivity />} />
-            <Route path="/trainer/profile" element={<ProfileManagement />} />
-            <Route path="/admin/addcourse" element={<AddCourse />} />
-            <Route path="/admin/addmodule" element={<AddModule />} />
-            <Route path="/admin/adduser" element={<AddUsers />} />
-            <Route path="/admin/userroletable" element={<UserRoleTable />} />
-            <Route path="/admin/course" element={<CourseManagement />} />
-            <Route path="/admin/course/module" element={<ModuleManagement />} />
-            <Route path="/trainer/dashboard" element={<Dashboard />} />
-            <Route path="/trainer/quizresult" element={<QuizResult />} />
-            <Route path="/trainer/createquiz" element={<CreateQuiz />} />
-            <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          </Routes>
-        </div>
-=======
       <div className="full-screen">
         {/* 
           How to use ProtectedRoutes:
@@ -130,7 +79,6 @@ function AppContent() {
           <Route path="/admin/user-management" element={<AdminUserRole />} />
 
         </Routes>
->>>>>>> e04b104a9210cc186fc966415cd794a316074cea
       </div>
     </>
   );
