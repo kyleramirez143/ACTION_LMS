d2ad2b7cfe99d7d60830d448de02f552a620aa31
Cassandra Alyanna Co <cassandra.co@awsys-i.com>
2026-01-29 20:37:04 +0800

    trainee grade files

--- backend/routes/resultRoute.js
+++ backend/routes/resultRoute.js
@@ -9,6 +9,9 @@ const router = express.Router();
 // Route for submitting a quiz: Requires any valid user (trainee)
 router.post('/:id', protect, resultController.submitQuiz); 
 
+// Route for getting trainee grades: Requires 'Admin' or 'Trainer' role
+router.get('/trainee-grades', protect, checkRole(['Admin', 'Trainer']), resultController.getTraineeGrades);
+
 // Route for viewing all quiz results: Requires 'Admin' or 'Trainer' role
 router.get('/:id', protect, checkRole(['Admin', 'Trainer']), resultController.getQuizResults); 
 
